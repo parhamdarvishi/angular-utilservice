38298bf65d9f7c3bafc83daeed8246293eda33b5
parhamdarvishi <darvishiparham14@gmail.com>
2020-12-04 16:32:03 +0330

    Shuffle Function

--- src/_services/util-service.service.ts
+++ src/_services/util-service.service.ts
@@ -136,4 +136,26 @@ export class UtilServiceService {
       .navigateByUrl("/", { skipLocationChange: true })
       .then(() => this.router.navigate([uri]));
   }
+
+  public shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+    //How To Use This Function
+    // var arr = [2, 11, 37, 42];
+    // shuffle(arr);
+    // console.log(arr);
+  }
 }
